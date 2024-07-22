f94131298ac3e1749a674578c8c7bed267d354ac
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-22 17:47:55 +0530

    "Updated Sidebar component with new imports, added logo text, and dynamic sidebar links"

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,24 +1,42 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import { sidebarLinks } from "@/constants";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import React from "react";
 
-const Sidebar = ({user}:SiderbarProps) => {
+const Sidebar = ({ user }: SiderbarProps) => {
+  const pathname = usePathname();
   return (
-    <section className='sidebar'>
+    <section className="sidebar">
       <nav className="flex flex-col gap-4">
-        <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
-        <Image 
-        src="/icons/logo.svg"
-        width={34}
-        height={34}
-        alt='Blink Logo'
-        className='size-[24px] max-xl:size-14'
-        />
+        <Link href="/" className="mb-12 cursor-pointer items-center gap-2">
+          <Image
+            src="/icons/logo.svg"
+            width={34}
+            height={34}
+            alt="Blink Logo"
+            className="size-[24px] max-xl:size-14"
+          />
+          <h1 className="sidebar-logo">Blink</h1>
         </Link>
-        <h1></h1>
+        {sidebarLinks.map((item) => {
+          const isActive =
+            pathname === item.route || pathname.startsWith(`${item.route}/`);
+          return (
+            <Link
+              href={item.route}
+              key={item.label}
+              className={cn("sidebar-link", { "bg-bank-gradient": isActive })}
+            >
+              {item.label}
+            </Link>
+          );
+        })}
       </nav>
     </section>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
