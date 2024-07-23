60f22cf83af092ab00eec8edd9245f0f62a4bf3d
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-23 14:16:32 +0530

    implement custom routes

---
+++ components/AuthForm.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AuthForm = ({type}: {type: String}) => {
+  return (
+    <section className='auth-form'>
+      <header className='flex flex-col gap-5md:gap-8'>
+        
+      </header>
+    </section>
+  )
+}
+
+export default AuthForm
