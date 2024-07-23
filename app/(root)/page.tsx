ab2c78353e2b3ad2ed9f4914d16a3a1ec3255f00
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-23 12:23:50 +0530

    "Added RightSidebar component and updated loggedIn object with additional properties"

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
-  const loggedIn = { firstName: "Chirag" };
+  const loggedIn = { firstName: "Chirag" , lastName: 'S Kotian',email:'kotian117@gmail.com' };
   return (
     <section className="home">
       <div className="home-content">
@@ -20,7 +21,9 @@ const Home = () => {
             totalCurrentBalance={17000}
           />
         </header>
+        RECENT TRANSSACTIONS
       </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
     </section>
   );
 };
