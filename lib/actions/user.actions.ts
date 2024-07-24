16d09557b0f76413cd7c2e49d01b7971a7440f73
Chirag-S-kotian <ckotian117@gmail.com>
2024-07-24 17:50:40 +0530

    "Updated .gitignore file, refactored AuthForm component, and modified authFormSchema in utils.ts"

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,19 @@
+"use server"
+export const signIn = async () =>{
+    try {
+        //fetch 
+    } catch (error) {
+        console.error('Error',error);
+        
+    }
+}
+
+
+export const signUp = async (userData:SignUpParams) =>{
+    try {
+        //fetch 
+    } catch (error) {
+        console.error('Error',error);
+        
+    }
+}
